850b0b2ef8f1c1b96bd5b0d28d487083ca0354fc
JoaoVictorSou <soulima.joao@gmail.com>
2023-09-04 21:21:35 -0300

    feat: register vatiable expense

--- src/screen/Movements/Movements.jsx
+++ src/screen/Movements/Movements.jsx
@@ -1,21 +1,50 @@
-import { Text, TextInput, Input, Button } from "react-native";
-import React from "react";
-import MoneyInput from "../../componet/MoneyInput";
-import { insert_expanse, begin } from "../../utils/database";
+import { Text, TextInput, Input, Button, View } from "react-native";
+import React, {useEffect} from "react";
+import MoneyInput from "../../components/MoneyInput";
+import { insert_expense, begin } from "../../utils/database";
+
+const db = begin()
 
 const Movements = (props) => {
     const [money, onChangeText] = React.useState('0.00');
+    const [expenses, loadExpenses] = React.useState([])
+    const [description, onChangeDescription] = React.useState('')
     const question = 'O que andou comprando?'
-    const db = begin()
+
+    useEffect(() => {
+        db.transaction(tx => {
+            tx.executeSql("SELECT * FROM TB_EXPENSE", [], (tx, result) => {
+                if(result.rows) {
+                    let build_expenses_list = []
+                    
+                    for (let i = 0; i < result.rows.length; i++) {
+                        item = result.rows.item(i)
+            
+                        build_expenses_list.push(
+                            <View key={item['ID']}>
+                                <Text>{item['DESCRIPTION']}</Text>
+                                <Text>{item['VALUE']}</Text>
+                                <Text>{item['TYPE']}</Text>
+                            </View>
+                        )
+                    }
+                    
+                    loadExpenses(build_expenses_list)
+                }
+            })
+        })
+    })
+
     return (<>
         <MoneyInput question = {question} value={money} onChangeText={onChangeText} />
+        <TextInput value={description} onChangeText={onChangeDescription} />
         <Button 
-            title = 'Pra cima deles'
+            title = 'Informar'
             onPress={_ => {
-                insert_expanse(db, 'Teste', money, 'V')
-                console.log(money)
+                insert_expense(db, description, money, 1)
             }}
         />
+        {expenses}
     </>)
 }
 
--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ import SQLite from 'react-native-sqlite-storage';
 
 function begin() {
     const create_tb_expanse = `
-    CREATE TABLE IF NOT EXISTS TB_EXPANSE (
+    CREATE TABLE IF NOT EXISTS TB_EXPENSE (
         ID INTEGER PRIMARY KEY, 
         DESCRIPTION VARCHAR(500) NOT NULL,
         VALUE REAL NOT NULL,
@@ -28,27 +28,29 @@ function begin() {
     return db
 }
 
-function insert_expanse(db, description, value, type) {
-    const insert_tb_expanse = `
-        INSERT INTO TB_EXPANSE (
-            DESCRIPTION,
-            VALUE,
-            TYPE
-        ) VALUES (
-            '${description}',
-            ${value},
-            '${type}'
-        );
-    `
-
-    db.transaction(tx => {
-        tx.executeSql(    
-            insert_tb_expanse,
-            [],
-            (_) => {
-                tx.executeSql("SELECT ID, DESCRIPTION, VALUE, TYPE FROM TB_EXPANSE", [], (tx, result) => console.log(result.rows.item(1)))
-            })
-    })
+function insert_expense(db, description, value, type) {
+    if(description && value != 0) {
+        const insert_tb_expense = `
+            INSERT INTO TB_EXPENSE (
+                DESCRIPTION,
+                VALUE,
+                TYPE
+            ) VALUES (
+                '${description}',
+                ${value},
+                ${type}
+            );
+        `
+    
+        db.transaction(tx => {
+            tx.executeSql(    
+                insert_tb_expense,
+                [],
+                (_) => {
+                    
+                })
+        })
+    }
 }
 
-export {insert_expanse, begin}
+export {insert_expense, begin}
